d3545a6b63054a518cc80ca5e1324bc9e641bdc7
arnie b <arnold.bwaila@gmail.com>
2020-07-14 12:27:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,6 +13,7 @@ class App < Sinatra::Base
   post '/puppy' do 
     name = params[:name]
     breed = params[:breed]
+    age = 
         @puppy = Puppy.new(name,breed,age)
     erb :display_puppy
   end
